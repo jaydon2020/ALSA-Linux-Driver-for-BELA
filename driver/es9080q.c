05eeb514dd5e35248b27feaa7a832249fb62e359
jaydon2020 <jiande2020@protonmail.com>
2025-06-11 01:39:34 +0800

    Week 1: ASoC - es9080q - Add initial support for ES9080Q DAC Prepared initial es9080q.c and es9080q.h files to begin driver work.
    Signed-off-by: JianDe <jiande2020@protonmail.com>

---
+++ driver/es9080q.c
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * es9080q.c  --  ES9080Q ALSA SoC Audio driver
+ *
+ * Copyright 2025 BeagleBoard.org
+ *
+ * Author: Jian De <jiande2020@gmail.com>,
+ *         Giulio Moro
+ */
+
+#include "es9080q.h"
+
+MODULE_DESCRIPTION("ASoC ES9080Q driver");
+MODULE_AUTHOR("JianDe <jiande2020@gmail.com>");
+MODULE_LICENSE("GPL");
---
+++ driver/es9080q.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * es9080q.h  --  ES9080Q ALSA SoC Audio driver
+ */
+
+#ifndef _ES9080Q_H
+#define _ES9080Q_H
+
+#endif
